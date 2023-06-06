fe0f640b0f7ba526be7c1ed67817f11e374a9f50
NikDoe <nikd0e@yandex.ru>
2023-06-06 18:37:01 +0300

    feat: create new user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,14 @@ export class UsersController {
   async findAll(@Res() response: Response): Promise<void> {
     const users = await this.usersService.getAllUsers();
     response.json({ message: 'получены все пользователи', users });
+  }
+
+  @Post('signup')
+  async createNewUser(
+    @Body() createUserDto: CreateUserDto,
+    @Res() response: Response,
+  ) {
+    const newUser = await this.usersService.createNewUser(createUserDto);
+    response.json({ message: 'регистрация прошла успешно', newUser });
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './entities/user.entity';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+
+import * as bcrypt from 'bcrypt';
+
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -8,5 +12,41 @@ export class UsersService {
 
   async getAllUsers(): Promise<User[]> {
     return this.userModel.findAll();
+  }
+
+  async findOneUser(where: {
+    id?: string;
+    username?: string;
+    email?: string;
+  }): Promise<User> {
+    return this.userModel.findOne({ where });
+  }
+
+  async createNewUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new User();
+
+    const existingByUserName = await this.findOneUser({
+      username: createUserDto.username,
+    });
+
+    if (existingByUserName) {
+      throw new ConflictException('Пользователь с таким именем уже существует');
+    }
+
+    const existingByEmail = await this.findOneUser({
+      email: createUserDto.email,
+    });
+
+    if (existingByEmail) {
+      throw new ConflictException('Пользователь с таким email уже существует');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    newUser.username = createUserDto.username;
+    newUser.password = hashedPassword;
+    newUser.email = createUserDto.email;
+
+    return newUser.save();
   }
 }
