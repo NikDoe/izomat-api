22524f02f43c0bb885de2a8c31f02813ffaf624d
NikDoe <nikd0e@yandex.ru>
2023-06-14 20:39:26 +0300

    refactor: add validation decorators for create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,27 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @ApiProperty({ example: 'username' })
+  @Matches(/^([^0-9]*)$/, { message: 'имя не должно содержать цифры' })
+  @MaxLength(20, { message: 'максимальная длинна имени 20 символов' })
+  @MinLength(2, { message: 'имя не должно быть кароче 2 символов' })
+  @IsNotEmpty({ message: 'имя пользователя обязательно для заполнения' })
   readonly username: string;
 
-  @IsNotEmpty()
+  @ApiProperty({ example: 'PDPOKDpkdp89723' })
+  @IsEmail({}, { message: 'невалидный email' })
+  @IsNotEmpty({ message: 'email обязателен для заполнения' })
   readonly password: string;
 
-  @IsNotEmpty()
+  @ApiProperty({ example: 'username@email.com' })
+  @MinLength(6, { message: 'пароль не должен быть кароче 6 символов' })
+  @IsNotEmpty({ message: 'пароль обязателен для заполнения' })
   readonly email: string;
 }
