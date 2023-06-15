4642524b354dc93d2242ff5b5f8c605c03b9bb3d
NikDoe <nikd0e@yandex.ru>
2023-06-15 10:46:58 +0300

    feat: creating and connecting a model

--- src/glasses/entities/glass.entity.ts
+++ src/glasses/entities/glass.entity.ts
@@ -1,1 +1,28 @@
-export class Glass {}
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table
+export class Glass extends Model {
+  @Column({ defaultValue: 0 })
+  price: number;
+
+  @Column
+  vendor_code: string;
+
+  @Column
+  name: string;
+
+  @Column
+  description: string;
+
+  @Column
+  images: string;
+
+  @Column({ defaultValue: 0 })
+  in_stock: number;
+
+  @Column({ defaultValue: false })
+  new: boolean;
+
+  @Column
+  popularity: number;
+}
--- src/glasses/glasses.module.ts
+++ src/glasses/glasses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GlassesService } from './glasses.service';
 import { GlassesController } from './glasses.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Glass } from './entities/glass.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Glass])],
   controllers: [GlassesController],
   providers: [GlassesService],
 })
