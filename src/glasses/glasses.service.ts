4dec53bcd45b347c4b38c85e9681fc435477f530
NikDoe <nikd0e@yandex.ru>
2023-06-15 11:17:32 +0300

    refactor: integrated the glass model into the service

--- src/glasses/glasses.service.ts
+++ src/glasses/glasses.service.ts
@@ -1,27 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
 import { CreateGlassDto } from './dto/create-glass.dto';
 import { UpdateGlassDto } from './dto/update-glass.dto';
+
 import { Glass } from './entities/glass.entity';
 
 @Injectable()
 export class GlassesService {
+  constructor(@InjectModel(Glass) private glassModule: typeof Glass) {}
   async create(createGlassDto: CreateGlassDto): Promise<Glass> {
-    return {};
+    const newGlass = await this.glassModule.create({ ...createGlassDto });
+    return newGlass;
   }
 
   async findAll(): Promise<Glass[]> {
-    return [];
+    return await this.glassModule.findAll();
   }
 
   async findOne(id: number): Promise<Glass> {
-    return {};
+    const existedGlass = await this.glassModule.findOne({ where: { id } });
+    return existedGlass;
   }
 
   async update(id: number, updateGlassDto: UpdateGlassDto): Promise<Glass> {
-    return {};
+    await this.glassModule.update({ ...updateGlassDto }, { where: { id } });
+    const updatedGalss = await this.glassModule.findByPk(id);
+    return updatedGalss;
   }
 
   async remove(id: number): Promise<Glass> {
-    return {};
+    const removed = await this.glassModule.findByPk(id);
+    await this.glassModule.destroy({ where: { id } });
+    return removed;
   }
 }
