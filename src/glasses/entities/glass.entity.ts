7e1c2a86d0db11d321b632b49806fd33cb65931f
NikDoe <nikd0e@yandex.ru>
2023-06-30 19:43:48 +0300

    feat: add data types to glass entity
    close #1

--- src/glasses/entities/glass.entity.ts
+++ src/glasses/entities/glass.entity.ts
@@ -1,28 +1,28 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 @Table
 export class Glass extends Model {
-  @Column({ defaultValue: 0 })
+  @Column({ defaultValue: 0, type: DataType.INTEGER })
   price: number;
 
-  @Column
+  @Column({ type: DataType.STRING, allowNull: false })
   vendor_code: string;
 
-  @Column
+  @Column({ type: DataType.STRING, allowNull: false })
   name: string;
 
-  @Column
+  @Column({ type: DataType.STRING, allowNull: false })
   description: string;
 
-  @Column
+  @Column({ type: DataType.STRING })
   images: string;
 
-  @Column({ defaultValue: 0 })
+  @Column({ defaultValue: 0, type: DataType.INTEGER })
   in_stock: number;
 
-  @Column({ defaultValue: false })
+  @Column({ defaultValue: false, type: DataType.BOOLEAN })
   new: boolean;
 
-  @Column
+  @Column({ type: DataType.INTEGER })
   popularity: number;
 }
