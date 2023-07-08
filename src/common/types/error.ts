2d626e184fcc9d18bef9a7eb6abc8b8a6caa152a
NikDoe <nikd0e@yandex.ru>
2023-07-08 17:19:32 +0300

    refactor: add description in ```error resonse type```

--- src/common/types/error.ts
+++ src/common/types/error.ts
@@ -12,21 +12,33 @@ class ErrorResponseType {
 export class BadRequestErrorType extends ErrorResponseType {
   @ApiProperty({ example: HttpStatus.BAD_REQUEST })
   statusCode: number;
+
+  @ApiProperty({ example: 'неверные данные' })
+  message: string;
 }
 
 export class ConflictErrorType extends ErrorResponseType {
   @ApiProperty({ example: HttpStatus.CONFLICT })
   statusCode: number;
+
+  @ApiProperty({ example: 'Введенные данные уже существуют' })
+  message: string;
 }
 
 export class UnauthorizedErrorType extends ErrorResponseType {
   @ApiProperty({ example: HttpStatus.UNAUTHORIZED })
   statusCode: number;
+
+  @ApiProperty({ example: 'Вы не авторизованы' })
+  message: string;
 }
 
 export class ForbiddenErrorType extends ErrorResponseType {
   @ApiProperty({ example: HttpStatus.FORBIDDEN })
   statusCode: number;
+
+  @ApiProperty({ example: 'Доступ запрещен' })
+  message: string;
 }
 
 export class NotFoundErrorType extends ErrorResponseType {
